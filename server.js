a1143aaf3886c23aeb55e61ba278ab39e17c3ece
Obada Athamneh <obadaobaida@gmail.com>
2021-04-10 17:57:18 +0300

    This is the design for header and footer

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
- 
+"use strict";
+require('dotenv').config();
+const express = require('express');
+const superagent = require('superagent');
+
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3004;
+const methodOverride = require('method-override');
+
+const pg = require('pg');
+//const dbClient = new pg.Client(process.env.DATABASE_URL)
+//dbClient.connect();
+app.use(methodOverride('_methode'))
+app.use(express.static('./public/styles'));
+app.use(express.static('./public/js'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set("views", ".")
+app.get('/', (req, res) => {
+
+    res.render('pages/event/index');
+});
+
+app.get('*', (req, res) => {
+    res.send('Not found')
+});
+
+const errorHandler = (err, req, res) => {
+    console.log('err', err);
+    res.status(500).render('pages/error', { err: err });
+};
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
